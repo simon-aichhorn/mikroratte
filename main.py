63dafa61b01c6f09fbb933a2738de3d46992d0b7
Julian <julihei52@gmail.com>
2023-11-18 00:58:06 +0100

    add some sleeping time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append('Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server')
 from drive import Drive
 
@@ -7,9 +8,11 @@ try:
     
     for i in range(0, 3):
         drive.driveNextField()
+        time.sleep(0.5)
         
     for i in range(0, 2):
         drive.driveBackField()
+        time.sleep(0.5)
     
 except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
     drive.stop()
