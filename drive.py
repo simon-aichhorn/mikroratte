475b0b81a89cfbe56320bd02caac6735bcfdc7ef
Julian <julihei52@gmail.com>
2023-11-17 22:07:38 +0100

    try new rotation

--- drive.py
+++ drive.py
@@ -2,35 +2,27 @@ import sys
 sys.path.append('../Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server')
 import time
 from Motor import Motor
+from Ultrasonic import Ultrasonic
 
 class Drive:
     def __init__(self):
         self.motor=Motor()
+        self.ultrasonic=Ultrasonic()
         
     def stop(self):
         self.motor.setMotorModel(0,0,0,0)
         
     def rotateLeft(self):
-        leftFront=0
-        leftBack=0
-        rightFront=2000
-        rightBack=2000
+        self.motor.Rotate(0)
         
-        count=0
-        while(count < 10):
-            count += 1
-            self.motor.setMotorModel(leftFront, leftBack, rightFront, rightBack)
-            time.sleep(0.5)
-            
-            if(leftFront == 0):
-                leftFront=-2000
-                leftBack=-2000
-                rightFront=0
-                rightBack=0
-            else:
-                leftFront=0
-                leftBack=0
-                rightFront=2000
-                rightBack=2000
-            
+        lastMean = 0 # average distance of last 5 measures
+        currentMean = 0 # average distance of the current 5 measures
+        
+        distanceCollection=[]
+        while(lastMean > currentMean):
+            for x in range(0,6):
+                distanceCollection.append(self.ultrasonic.get_distance())
+                time.sleep(0.1)
+        
+        
         self.stop()
