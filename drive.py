17966df9e404f623651cd958558bcd60936c6616
Nico KNÜNZ <Nico.KNUENZ@bachmann.info>
2023-11-18 21:45:18 +0100

    test

--- drive.py
+++ drive.py
@@ -16,14 +16,6 @@ class Drive:
         self.initIR()
         self.pwm_S=Servo()
         self.rfw=Rfw()
-
-        self.still_driving=False
-        self.leftDistance=0 # distance to left wall
-        self.rightDistance=0 # distance to right wall
-        self.frontDistance=12 # distance to front
-
-        correctingDriveThread = threading.Thread(target = self.correctDrive)
-        correctingDriveThread.start()
         
     def initIR(self):
         self.IR01 = 14
@@ -34,22 +26,17 @@ class Drive:
         GPIO.setup(self.IR02,GPIO.IN)
         GPIO.setup(self.IR03,GPIO.IN)
         
-    def slowForward(self):
-        while not self.stop_driving.is_set():
-            wallDifference = self.leftDistance - self.rightDistance # positive difference = correct to left | negative difference = correct to right
-            factor = 17
+    def slowForward(self, distances):
+        wallDifference = distances[0] - distances[2] # positive difference = correct to left | negative difference = correct to right
+        factor = 17
 
-            nV = 750
-            LW = nV - (wallDifference * factor)
-            RW = nV + (wallDifference * factor)
-            print(LW, RW)
-            self.motor.setMotorModel(LW,LW,RW,RW)
-            time.sleep(0.25)
-            self.motor.setMotorModel(0,0,0,0)
-            time.sleep(0.1)
+        nV = 750
+        LW = nV - (wallDifference * factor)
+        RW = nV + (wallDifference * factor)
 
-            if(self.frontDistance < 12):
-                self.stop_driving.set()
+        print(LW, RW)
+        self.motor.setMotorModel(LW,LW,RW,RW)
+
         
     def slowBackward(self):
         self.rfw.startRFW()
@@ -61,21 +48,16 @@ class Drive:
         self.motor.setMotorModel(0,0,0,0)
         
     def driveNextField(self):
-        # create stopping event
-        self.stop_driving = threading.Event()
+        while(not isOnLine()):
+            distances=checkDistances()
 
-        # create forwarding driving thread
-        driveForwardThread = threading.Thread(target= self.slowForward)
-        
-        driveForwardThread.start()
+            # check if we are standing in front of a wall
+            if(distances[1] < 8):
+                break
 
-        #some sleep to drive away from line
-        time.sleep(0.5)
-        
-        self.waitForLine()
-
-        # stop threads
-        self.stop_driving.set()
+            self.slowForward(distances)
+            time.sleep(0.5)
+            self.stop()
         
     def driveBackField(self):
         self.slowBackward()
@@ -85,15 +67,8 @@ class Drive:
         
         self.waitForLine()
         
-    def waitForLine(self):
-        allActive = False
-        # check if all ir are active
-        while(not allActive):
-            if(self.getIRState() == 7):
-                allActive = True
-        
-        print("Found line!")
-        self.stop()
+    def isOnLine(self):
+        return self.getIRState() == 7
 
     def rotateRight(self):
         self.motor.setMotorModel(1500,1500,-1500,-1500)
@@ -117,26 +92,22 @@ class Drive:
         
         return bitcoded
 
-    def correctDrive(self):
+    def checkDistances(self):
+        distances=(0,0,0)
 
-        while(True):
-            for i in range(90,30,-60):
-                self.pwm_S.setServoPwm('0',i)
-                time.sleep(0.1)
-                if i==30:
-                    self.leftDistance = self.ultrasonic.get_distance() % 33
-                elif i==90:
-                    test = self.ultrasonic.get_distance()
-                    self.frontDistance = test
-                else:
-                    self.rightDistance = self.ultrasonic.get_distance()  % 33
-            for i in range(0,181,90):
-                self.pwm_S.setServoPwm('0',i)
-                time.sleep(0.1)
-                if i==0:
-                    self.leftDistance = self.ultrasonic.get_distance() % 33
-                elif i==90:
-                    test = self.ultrasonic.get_distance()
-                    self.frontDistance = test
-                else:
-                    self.rightDistance = self.ultrasonic.get_distance() % 33
+        #read left
+        self.pwm_S.setServoPWM('0', 0)
+        time.sleep(0.2)
+        distances=(self.ultrasonic.get_distance(), 0, 0)
+
+        #read mid
+        self.pwm_S.setServoPWM('0', 90)
+        time.sleep(0.2)
+        distances=(distances[0], self.ultrasonic.get_distance(), 0)
+
+        #read right
+        self.pwm_S.setServoPWM('0', 180)
+        time.sleep(0.2)
+        distances=(distances[0], distances[1], self.ultrasonic.get_distance())
+
+        return checkDistances
